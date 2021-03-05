e67404be1867742f9ae002309add72f22d70af4a
Charlie Laver <charlielaver7@gmail.com>
2021-03-05 10:02:42 +0000

    node-server

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+const SMTPTransport = require('nodemailer/lib/smtp-transport');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors());
+
+app.get('/',(req, res)=>{
+    res.send('welcome to my portfolio')
+})
+
+app.post('/', (req,res)=>{
+    let data = req.body
+    let SMTPTransport = nodemailer.createTransport({
+        service:'Gmail',
+        port:465,
+        auth:{
+            user:'username', //email username and password
+            pass: 'password'
+        }
+    });
+
+    let mailOptions={
+        from:data.email,
+        to:'lankydragonqueries@gmail.com',
+        subject: `Message from ${data.name}`,
+        html:`
+        
+        <h3>Info</h3>
+         <ul>
+          <li>Name: ${data.name}</li>
+          <li>Email: ${data.email}</li>
+          <li>Subject: ${data.subject}</li>
+        </ul>
+
+          <h3>Message</h3>
+          <p>${data.message}</p>
+
+        `
+    };
+
+    SMTPTransport.sendMail(mailOptions, (error, response) => {
+
+        if(error){
+            res.send(error)
+        } else {
+            res.send('Success')
+        }
+    })
+
+    SMTPTransport.close();
+})
+
+const PORT = process.env.PORT||3001;
+app.listen(PORT,()=>{
+    console.log(`server starting at port ${PORT}`);
+})
